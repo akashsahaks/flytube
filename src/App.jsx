86ac0ead1840de49c259786cbedfc362392e5aa8
akash <akashsah.aks@gmail.com>
2023-08-25 23:30:21 +0530

    basic setup done created basic navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Body from "./components/Body";
+import Head from "./components/Head";
+
+function App() {
+   return (
+      <>
+         <Head />
+         <Body />
+      </>
+   );
+}
+
+export default App;
---
+++ src/components/Head.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Head = () => {
+   return (
+      <>
+         <div className="grid grid-flow-col p-5 shadow-lg">
+            <div className="flex col-span-1">
+               <div>
+                  <i class="fa-solid fa-bars fa-2xl"></i>
+               </div>{" "}
+               <img
+                  className="h-8 mx-3 pb-2"
+                  src="https://www.seekpng.com/png/full/77-772362_youtube-logo-youtube-logo-png.png"
+                  alt="youtube logo"
+               />
+            </div>
+            {/* ----- search bar ----- */}
+            <div className="col-span-10 px-10">
+               <input
+                  className="w-1/3 border border-gray-500 rounded-l-full p-1"
+                  type="text"
+                  name="search"
+               />
+               <button className="border border-gray-400 rounded-r-full p-1 px-4">
+                  Search
+               </button>
+            </div>
+            <div className="col-span-1">
+               <i class="fa-solid fa-user"></i>
+            </div>
+         </div>
+      </>
+   );
+};
+
+export default Head;
