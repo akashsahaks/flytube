01c18a2ca524245a7a4686aefc5ad10796e0094e
akash <akashsah.aks@gmail.com>
2023-08-27 00:58:00 +0530

    added maxResult for getting maxximum videos limit is 50

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,5 +1,5 @@
 const GOOGLE_API_KEYS = "AIzaSyCsf9ofNLhXcQchkGkvzd2qiSO3ER4f2A4";
 
 export const YOUTUBE_VIDEOS_API =
-   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=" +
+   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" +
    `${GOOGLE_API_KEYS}`;
